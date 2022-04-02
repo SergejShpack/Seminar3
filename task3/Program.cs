e4a7e118d780edcb7329358d7ce2c83c93ba2e45
«Segej» <nikolaev.sergej2018@yandex.com>
2022-04-02 12:48:00 +0300

    t3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Определить количество цифр в числе"
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+int count = 0;
+int i = num;
+  while (i > 0)
+{
+    i = i / 10;
+    count++;
+}
+Console.Write("Количество цифр в числе " + num + " равно " + count);
