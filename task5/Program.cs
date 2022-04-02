39c6607f4856e34f9e44a3ccd3d1b7477e8ee41b
«Segej» <nikolaev.sergej2018@yandex.com>
2022-04-02 13:12:25 +0300

    t5

---
+++ task5/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+int[] array = new int[8];
+Random random = new Random();
+for (int i = 0; i < array.Length - 1; i++)
+{
+    array[i] = random.Next(0, 2);
+    Console.Write(array[i] + " ");
+}
