1d829460d4bce1adb104023d45c88f041b6687d0
«Segej» <nikolaev.sergej2018@yandex.com>
2022-04-02 12:55:05 +0300

    t4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+             //Написать программу вычисления произведения чисел от 1 до N"
+
+Console.Write("Введите число: N = ");
+int N = int.Parse(Console.ReadLine());
+
+int number = 1;
+int factorial = 1;
+    while (number <= N)
+{
+    factorial = factorial * number;
+    number++;
+}
+Console.Write("Произведение чисел от 1 до N равно " + factorial);
