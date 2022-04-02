bd962fc6a256d29bc22f6bd0f868f5e5302ff0c3
«Segej» <nikolaev.sergej2018@yandex.com>
2022-04-02 12:26:15 +0300

    t1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//     Найти кубы чисел от 1 до N
+
+Console.WriteLine(" Найти кубы чисел от 1 до N");
+
+
+Console.Write("Введите N (от 1 до 255): ");
+byte N = Convert.ToByte(Console.ReadLine());
+
+
+     for (int i = 1; i <= N; ++i)
+{
+    string text = " \n";
+    text = text + "   " + i + " \t| " + i * i * i;
+    Console.WriteLine(text);
+}
