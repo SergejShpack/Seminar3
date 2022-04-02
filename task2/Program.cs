0a9f34cc39f93c24fb62301cae599652e0004550
«Segej» <nikolaev.sergej2018@yandex.com>
2022-04-02 12:38:12 +0300

    t2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+      //Найти сумму чисел от 1 до А"
+
+Console.Write("Введите натуральное число А = ");
+int A = int.Parse(Console.ReadLine());
+
+int counter = 1;
+int sum = 0;
+    while (counter <= A)
+    { 
+    sum = sum + counter;
+    counter++;
+    }
+
+Console.Write("Сумма чисел от 1 до " + (counter - 1) + " равна " + sum);
